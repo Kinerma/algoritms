778063c857e1a4a399f1d013eafebe77cefc0155
Maksim Bakhirev <m.bahirew@yandex.ru>
2023-04-24 20:43:31 +0300

    Стек и очередь - верстка кнопок

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import queueStyle from './queue-page.module.css'
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
+import {SHORT_DELAY_IN_MS} from "../../constants/delays";
+import {ElementStates} from "../../types/element-states";
 
 export const QueuePage: React.FC = () => {
   return (
     <SolutionLayout title="Очередь">
-
+      <form className={queueStyle.form}>
+        <Input type='text'
+               maxLength={4}
+               isLimitText
+               placeholder='Введите значение' />
+        <Button type='submit'
+                text='Добавить' />
+        <Button type='submit'
+                text='Удалить' />
+        <Button type='submit'
+                text='Очистить'
+                extraClass={queueStyle.button} />
+      </form>
     </SolutionLayout>
   );
 };
--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import stackStyle from './stack-page.module.css'
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
+import {SHORT_DELAY_IN_MS} from "../../constants/delays";
+import {ElementStates} from "../../types/element-states";
 
 export const StackPage: React.FC = () => {
   return (
     <SolutionLayout title="Стек">
-
+      <form className={stackStyle.form}>
+        <Input type='text'
+               maxLength={4}
+               isLimitText />
+        <Button type='submit'
+                text='Добавить' />
+        <Button type='submit'
+                text='Удалить' />
+        <Button type='submit'
+                text='Очистить'
+                extraClass={stackStyle.button} />
+      </form>
     </SolutionLayout>
   );
 };
