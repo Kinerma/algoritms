372d111090de2f3e50b28749fb1ee8741f9efb91
Maksim Bakhirev <m.bahirew@yandex.ru>
2023-05-02 20:10:19 +0300

    верстка списка

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import listStyle from './list-page.module.css'
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
+import {ArrowIcon} from "../ui/icons/arrow-icon";
 
 export const ListPage: React.FC = () => {
   return (
     <SolutionLayout title="Связный список">
-
+      <form className={listStyle.form}>
+        <div className={listStyle.value}>
+          <Input placeholder='Введите значение'/>
+          <Button text='Добавить в head'
+                  extraClass={listStyle.valueButton}  />
+          <Button text='Добавить в tail'
+                  extraClass={listStyle.valueButton}  />
+          <Button text='Удалить в head'
+                  extraClass={listStyle.valueButton}  />
+          <Button text='Удалить в tail'
+                  extraClass={listStyle.valueButton}  />
+        </div>
+        <div className={listStyle.index}>
+          <Input placeholder='Введите индекс' />
+          <Button text='Добавить по индексу'
+                  extraClass={listStyle.indexButton}  />
+          <Button text='Удалить по индексу'
+                  extraClass={listStyle.indexButton} />
+        </div>
+      </form>
+      <div className={listStyle.circles}>
+        <div>
+          <Circle />
+          <ArrowIcon />
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
