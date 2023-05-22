347d7e05c60e01df3c59450a7b6c74e04f98729f
Maksim Bakhirev <m.bahirew@yandex.ru>
2023-05-23 01:11:20 +0300

    e2e queue test

--- cypress/constants.ts
+++ cypress/constants.ts
@@ -1,3 +1,5 @@
 export const defaultColor = 'rgb(0, 50, 255)';
 export const circle = '[class*=circle_circle]';
-export const changingColor = 'rgb(210, 82, 225)'
+export const changingColor = 'rgb(210, 82, 225)';
+
+
---
+++ cypress/e2e/queue.spec.cy.js
@@ -0,0 +1,38 @@
+import {circle, defaultColor, changingColor, circle_content} from "../constants";
+
+describe('Тестирование Очередь', () => {
+  beforeEach(() => {
+    cy.visit("/queue");
+  });
+
+  it('Если в инпуте пусто, то кнопка добавления недоступна', () => {
+    cy.get("input").should("be.empty")
+    cy.contains("Добавить").should("be.disabled")
+  })
+
+  it('Проверка правильности добавления элемента в очередь', () => {
+
+  })
+
+  it('Проверка правильности удаления элемента из очереди', () => {
+
+  })
+
+  it('Проверка поведения кнопки «Очистить».', () => {
+    cy.clock()
+    cy.get("input").type("123")
+    cy.contains("Добавить").should("be.visible").click()
+    cy.wait(500)
+
+    cy.get("input").type("15")
+    cy.contains("Добавить").should("be.visible").click()
+    cy.wait(500)
+
+    cy.get("input").should("have.value", "")
+    cy.contains("Очистить").should("be.visible").click()
+    cy.get(circle).as("circle")
+    cy.get('@circle').should(($circle) => {
+      expect($circle).to.contain('').to.have.css("border-color", defaultColor)
+    })
+  })
+})
